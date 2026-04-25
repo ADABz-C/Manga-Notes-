6fcee1e9b0272791de4c008494cfb36d275f85f9
Chidi <adabanyadavid@gmail.com>
2026-04-25 02:48:35 -0400

    introduced OOP

--- components/index/index.js
+++ components/index/index.js
@@ -1,3 +1,13 @@
+class Book {
+    constructor(title, author, description, image, rank){
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.image = image;
+        this.rank = rank;
+    }
+}
+
 async function loadBooks(){
     const response = await fetch(`http://localhost:3000/books`)
     const books = await response.json();
@@ -33,3 +43,103 @@ loadBooks();
 
 
 //Implementing Search function
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn')
+
+
+let allbooks = [];
+
+
+function displayBooks(bookCards){
+
+    const bookObj = new Book(
+        book.title,
+        book.author,
+        book.description,
+        book.image,
+        book.rank
+    )
+
+    const bookShelf = document.getElementById('bookShelf');
+    bookShelf.innerHTML = "";
+
+    bookCards.forEach(book => {
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+        card.innerHTML = 
+        `
+            <div class="titleHolder"><h1>${book.title}</h1></div>
+                <div class="bookContent">
+                    <div class="imgHolder"><img src="${book.book_image}" alt="book image"></div>
+                    <div class="descriptionHolder">
+                        <p>
+                            ${book.description}
+                        </p>
+                    </div>
+                </div>
+                <div class="credentialHolder">
+                    <p>Author: ${book.author}</p>
+                    <br>
+                    <p>World Rank: ${book.rank} </p>
+                </div>
+        `;
+        
+        bookShelf.appendChild(card);
+
+        // Card animation
+
+        const cards = document.querySelectorAll('.book-card');
+
+        cards.forEach(card => {
+
+            //mouse enters
+            card.addEventListener('mouseenter', () => {
+                gsap.to(card, {
+                    y: -5,
+                    duration: 0.1
+                });
+
+            })
+
+            // mouse leaves
+
+            card.addEventListener('mouseleave', () => {
+                gsap.to(card, {
+                    y:0,
+                    duration: 0.1
+                });
+            });
+        });
+
+    });
+
+}
+
+function searchBooks(query){
+    const filtered = allbooks.filter(book => 
+        book.title.toLowerCase().includes(query.toLowerCase())
+    )
+
+    displayBooks(filtered)
+
+}
+
+searchInput.addEventListener('input', (e)=> {
+    searchBooks(e.target.value);
+});
+
+
+async function loadSearchedBooks() {
+    const response = await fetch("http://localhost:3000/books");
+    const books = await response.json();
+
+    allbooks = books;
+    displayBooks(allbooks);
+}
+
+searchBtn.addEventListener('click', (e)=>{
+    searchBooks(searchInput.value);
+})
+
+
+loadSearchedBooks();
--- components/server/server.js
+++ components/server/server.js
@@ -14,14 +14,14 @@ const PORT = 3000;
 app.get('/books', async (req, res) => {
     try{
         const API_KEY = process.env.NY_BOOKSAPI_KEY;
-        const BASE_URL = process.env.NY_BOOKSAPI_BASE_URL
+        // const BASE_URL = process.env.NY_BOOKSAPI_BASE_URL
 
-        const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_KEY}`);
+        const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/graphic-books-and-manga.json?api-key=${API_KEY}`);
 
         const data = await response.json();
 
         // breaking through layers
-        const books = data.results.books.slice(0,9);
+        const books = data.results.books.slice(0,15);
 
         res.json(books);
 
